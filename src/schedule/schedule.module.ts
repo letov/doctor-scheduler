fa907f6c512eab182f399e05a7f5f0180ed9bde3
Руслан Карымов <ruslan@eljur.team>
2023-04-13 16:09:08 +0300

    cs fix

--- src/schedule/schedule.module.ts
+++ src/schedule/schedule.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ScheduleService } from './schedule.service';
 import { ScheduleController } from './schedule.controller';
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { Customer } from "../customer/entities/customer.entity";
-import { Schedule } from "./entities/schedule.entity";
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Customer } from '../customer/entities/customer.entity';
+import { Schedule } from './entities/schedule.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Schedule, Customer])],
